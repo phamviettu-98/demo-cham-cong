e23379172f3b5bbf24b6b5b64ae108011a1cbbd6
quytladean <phamviettu567@gmail.com>
2022-05-07 01:15:22 +0700

    cal lat & lon

--- src/main/java/com/example/demo/service/CheckInService.java
+++ src/main/java/com/example/demo/service/CheckInService.java
@@ -33,5 +33,20 @@ public class CheckInService {
 
 		return new HttpApiResponse(200, "CheckIn success", null);
 	}
+	
+	///20.99661819507934, 105.79335911389619 fun
+
+	public static double distanceBetween2Points(double la1, double lo1, double la2, double lo2) {
+		double R = 6371;
+		double dLat = (la2 - la1) * (Math.PI / 180);
+		double dLon = (lo2 - lo1) * (Math.PI / 180);
+		double la1ToRad = la1 * (Math.PI / 180);
+		double la2ToRad = la2 * (Math.PI / 180);
+		double a = Math.sin(dLat / 2) * Math.sin(dLat / 2)
+				+ Math.cos(la1ToRad) * Math.cos(la2ToRad) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
+		double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+		double d = R * c;
+		return d;
+	}
 
 }
